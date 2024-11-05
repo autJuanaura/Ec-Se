173633a7fcf09de7749959e384ad4c443da1d251
juanbig <juannodecoorp@gmail.com>
2024-11-04 23:44:22 -0500

    add product By name using contains(text() *[-|-]*

---
+++ src/main/java/com/devaura/qa/EcommerceAddByName.java
@@ -0,0 +1,108 @@
+package com.devaura.qa;
+
+import java.nio.file.Paths;
+import java.time.Duration;
+
+import org.junit.Assert; 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class EcommerceAddByName implements Runnable {
+
+	private String browser;
+	private static final String PRODUCT_NAME = "Sauce Labs Fleece Jacket";
+
+	public EcommerceAddByName(String browser) {
+		this.browser = browser;
+	}
+
+	public static void main(String[] args) {
+		int numberOfThreads = 1;
+		String browser = "chrome";
+
+		for (int i = 0; i < numberOfThreads; i++) {
+			Thread thread = new Thread(new EcommerceAddByName(browser));
+			thread.start();
+		}
+	}
+
+	@Override
+	public void run() {
+	    WebDriver driver = getDriver(browser);
+	    if (driver != null) {
+	        try {
+	            driver.get("https://www.saucedemo.com/");
+	            driver.manage().window().maximize();
+	            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+	            WebElement userNameField = driver.findElement(By.id("user-name"));
+	            WebElement psswdField = driver.findElement(By.id("password"));
+	            WebElement loginButton = driver.findElement(By.id("login-button"));
+	            userNameField.sendKeys("standard_user");
+	            psswdField.sendKeys("secret_sauce");
+	            loginButton.click();
+	            Thread.sleep(2000);
+	            clickOnProductByName(driver, PRODUCT_NAME);
+	            Thread.sleep(2000);
+	            WebElement addToCarButton = driver.findElement(By.id("add-to-cart"));
+	            addToCarButton.click();
+	            Thread.sleep(4000);
+	            WebElement shoppingCartContainer = driver.findElement(By.id("shopping_cart_container"));
+	            Assert.assertTrue("Shopping cart container should be visible", shoppingCartContainer.isDisplayed());
+	            WebElement cartBadge = driver.findElement(By.className("shopping_cart_badge"));
+	            Assert.assertEquals("Shopping cart badge should be 1", "1", cartBadge.getText());
+	            addToCarButton = driver.findElement(By.id("add-to-cart"));
+	            String buttonText = addToCarButton.getText();
+	            Assert.assertEquals("Button text should be 'Remove'", "Remove", buttonText);
+	            String buttonColor = addToCarButton.getCssValue("background-color");
+	            Assert.assertEquals("Button color should be #e2231a", "rgba(226, 35, 26, 1)", buttonColor); // RGBA format
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        } finally {
+	            driver.quit();
+	        }
+	    } else {
+	        System.out.println("Driver not found for the specified browser.");
+	    }
+	}
+
+	private void clickOnProductByName(WebDriver driver, String productName) {
+		String xpath = String.format("//*[contains(text(),'%s')]", productName);
+		WebElement productElement = driver.findElement(By.xpath(xpath));
+		productElement.click();
+	}
+
+	private static WebDriver getDriver(String browser) {
+		WebDriver driver = null;
+		String driverPath = Paths.get("src/test/java/com/devaura/qa/drivers/chrome/chromedriver").toAbsolutePath()
+				.toString();
+
+		try {
+			switch (browser.toLowerCase()) {
+			case "chrome":
+				System.setProperty("webdriver.chrome.driver", driverPath);
+				ChromeOptions options = new ChromeOptions();
+				driver = new ChromeDriver(options);
+				break;
+
+			case "safari":
+				if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+					driver = new SafariDriver();
+				} else {
+					System.err.println("Safari is only supported on macOS.");
+				}
+				break;
+
+			default:
+				System.err.println("Browser not supported: " + browser);
+				break;
+			}
+		} catch (Exception e) {
+			System.err.println("Error initializing WebDriver: " + e.getMessage());
+		}
+		return driver;
+	}
+}
--- src/main/java/com/devaura/qa/EcommerceParallel.java
+++ src/main/java/com/devaura/qa/EcommerceParallel.java
@@ -1,92 +1,98 @@
 package com.devaura.qa;
 
+import java.nio.file.Paths;
 import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class EcommerceParallel implements Runnable {
 
-    private String browser;
+	private String browser;
 
-    public EcommerceParallel(String browser) {
-        this.browser = browser;
-    }
+	public EcommerceParallel(String browser) {
+		this.browser = browser;
+	}
 
-    public static void main(String[] args) {
-        // Specify the number of parallel threads you want to run
-        int numberOfThreads = 1; // Change this as needed
-        String browser = "chrome"; // Change to "safari" if needed
+	public static void main(String[] args) {
+		// Specify the number of parallel threads you want to run
+		int numberOfThreads = 1; // Change this as needed
+		String browser = "chrome"; // Change to "safari" if needed
 
-        for (int i = 0; i < numberOfThreads; i++) {
-            Thread thread = new Thread(new EcommerceParallel(browser));
-            thread.start();
-        }
-    }
+		for (int i = 0; i < numberOfThreads; i++) {
+			Thread thread = new Thread(new EcommerceParallel(browser));
+			thread.start();
+		}
+	}
 
-    @Override
-    public void run() {
-        WebDriver driver = getDriver(browser);
-        if (driver != null) {
-            try {
-                driver.get("https://www.saucedemo.com/");
-                driver.manage().window().maximize();
-                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+	@Override
+	public void run() {
+		WebDriver driver = getDriver(browser);
+		if (driver != null) {
+			try {
+				driver.get("https://www.saucedemo.com/");
+				driver.manage().window().maximize();
+				driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
 
-                WebElement userNameField = driver.findElement(By.id("user-name"));
-                WebElement psswdField = driver.findElement(By.id("password"));
-                WebElement loginButton = driver.findElement(By.id("login-button"));
+				WebElement userNameField = driver.findElement(By.id("user-name"));
+				WebElement psswdField = driver.findElement(By.id("password"));
+				WebElement loginButton = driver.findElement(By.id("login-button"));
 
-                userNameField.sendKeys("standard_user");
-                psswdField.sendKeys("secret_sauce");
-                loginButton.click();
-                Thread.sleep(2000);
+				userNameField.sendKeys("standard_user");
+				psswdField.sendKeys("secret_sauce");
+				loginButton.click();
+				Thread.sleep(2000);
 
-                WebElement firstProduct = driver.findElement(By.cssSelector(".inventory_item:first-child .inventory_item_name"));
-                firstProduct.click();
-                Thread.sleep(2000);
+				WebElement firstProduct = driver
+						.findElement(By.cssSelector(".inventory_item:first-child .inventory_item_name"));
+				firstProduct.click();
+				Thread.sleep(2000);
 
-                WebElement addCardToButton = driver.findElement(By.id("shopping_cart_link"));
-                addCardToButton.click();
-                Thread.sleep(2000);
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				// Close the driver
+				driver.quit();
+			}
+		} else {
+			System.out.println("Driver not found for the specified browser.");
+		}
+	}
 
-                WebElement cartIcon = driver.findElement(By.className("shopping_cart_link"));
-                cartIcon.click();
-                Thread.sleep(2000);
+	private static WebDriver getDriver(String browser) {
+		WebDriver driver = null;
+		String driverPath = Paths.get("src/test/java/com/devaura/qa/drivers/chrome/chromedriver").toAbsolutePath()
+				.toString();
 
-                WebElement cartItem = driver.findElement(By.cssSelector(".cart_item .inventory_item_name"));
-                System.out.println("Item in cart: " + cartItem.getText());
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                // Close the driver
-                driver.quit();
-            }
-        } else {
-            System.out.println("Driver not found for the specified browser.");
-        }
-    }
+		try {
+			switch (browser.toLowerCase()) {
+			case "chrome":
+				System.setProperty("webdriver.chrome.driver", driverPath);
+				ChromeOptions options = new ChromeOptions();
+				driver = new ChromeDriver(options);
+				break;
 
-    private static WebDriver getDriver(String browser) {
-        WebDriver driver = null;
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                System.setProperty("webdriver.chrome.driver", "/home/autmarisella/eclipse-workspace/eCommerce-flow/src/test/java/com/devaura/qa/drivers/chrome/chromedriver");
-                driver = new ChromeDriver();
-                break;
-            case "safari":
-                if (System.getProperty("os.name").toLowerCase().contains("mac")) {
-                    driver = new SafariDriver();
-                } else {
-                    System.out.println("Safari is only supported on macOS.");
-                }
-                break;
-            default:
-                System.out.println("Browser not supported: " + browser);
-                break;
-        }
-        return driver;
-    }
+			case "safari":
+				if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+					driver = new SafariDriver();
+				} else {
+					System.err.println("Safari is only supported on macOS.");
+				}
+				break;
+
+			default:
+				System.err.println("Browser not supported: " + browser);
+				break;
+			}
+		} catch (Exception e) {
+			System.err.println("Error initializing WebDriver: " + e.getMessage());
+		}
+
+		return driver;
+	}
 }
